bcef7f27c92d9785ab250abef7e175acd65cef33
Priyanka Nawalramka <priyanka.nawalramka@gmail.com>
2021-05-18 21:59:33 -0600

    update

--- check_availability.py
+++ check_availability.py
@@ -2,18 +2,17 @@ import argparse
 import asyncio
 from datetime import datetime, timezone, timedelta
 import json
-import os
 import requests
 import signal
 
-
-def check(dist_id, age):
+def check(dist_id, age) -> []:
 	url = 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict'
 	ist_date = datetime.now(timezone(timedelta(hours=5, minutes=30))).strftime("%d-%m-%Y")
 	try:
 		res = requests.get(
 			url,
-			headers={'accept':'applicaton/json', 'Accept-Language':'en_US', 'Authorization':'Bearer '+os.getenv('COWIN_TOKEN')},
+			headers={'accept':'applicaton/json', 'Accept-Language':'en_US',
+			'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0'},
 			params={'district_id':dist_id, 'date':ist_date}
 		)
 		res.raise_for_status()
@@ -27,6 +26,7 @@ def check(dist_id, age):
 		for session in center['sessions']:
 			if session['available_capacity'] > 0 and session['min_age_limit'] <= age:
 				available.append(center)
+				break
 
 	return available
 
@@ -42,7 +42,7 @@ def check_district(dist, age):
 
 
 def check_districts(dists, age):
-	[check(dist, age) for dist in dists]
+	[check_district(dist, age) for dist in dists]
 
 
 async def runner(dists, interval, age):
@@ -55,7 +55,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('-d', '--dists', type=int, nargs='+', help='district ids [-d 111 222 ...]')
 	parser.add_argument('-a', '--age', type=int, default=18, help='minimum age limit (18 or 45)')
-	parser.add_argument('-i', '--interval', type=int, default=5, help='retry interval')
+	parser.add_argument('-i', '--interval', type=int, default=10, help='retry interval')
 	args = parser.parse_args()
 
 	asyncio.run(runner(**vars(args)))
